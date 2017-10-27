60bfdc2b81bc4d44a7a8d160cc47e7a4779f2a80
Rajpal Singh <rajpalsinghsikar@users.noreply.github.com>
2017-10-27 13:35:39 +0000

    Update prime_number.py
    comments added in prime number program

--- prime_number.py
+++ prime_number.py
@@ -1,5 +1,8 @@
+# A Python program in Python to check if the number is prime or not
+# input from the user
 num = int(input("Enter a number: "))
 
+# prime numbers are greater than 1
 if num > 1:
    for i in range(2,num):
        if (num % i) == 0:
